24e97dfb4376524bad4d1675047eb818bf6dc851
sikle00 <92247756+sikle00@users.noreply.github.com>
2024-03-18 13:03:57 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let answers = ['The heaven says yes', 'Not likely', 'hell nah', 'hell yeah', 'Im not sure', 'not likely', 'you wish', 'unlucky', 'Its written in the stars', 'Go for it!', 'Sounds exciting'];
+let answer = '8';
+let classFont = 'font';
+
+updateView()
+function updateView(){
+    let html = '';
+    html += /*html*/` 
+    <div class="baller">
+        <button class="whiteCircle" onclick="answerMe()">
+            <div class="${classFont}">${answer}</div>
+        </button>
+    </div>
+    <input id="question" placeholder="Write question here">`
+    document.getElementById('app').innerHTML = html
+}
+function answerMe(){
+    let randomIndex = Math.floor(Math.random() * answers.length)
+    let ansTxt = answers.length > 0 ? answers[randomIndex] : 'Refresh the page to get more attempts'
+    classFont = '';
+    answer = ansTxt;
+    answers.splice(randomIndex, 1);
+    updateView();
+}
+
